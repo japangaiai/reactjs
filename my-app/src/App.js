c105fb0d325b8332a8f9bfe17a0509d6fbac528b
japangaiai <43401204+japangaiai@users.noreply.github.com>
2022-02-08 17:22:30 +0900

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter, Link } from "react-router-dom";
+
+import { Router } from "./router/Router";
+import './App.css';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className='App'>
+        <Link to="/">HOME</Link>
+        <br />
+        <Link to="/page1">Page1</Link>
+        <br />
+        <Link to="/page2">Page2</Link>
+      </div>
+      <Router />
+    </BrowserRouter>
+  );
+}
+
+export default App;
